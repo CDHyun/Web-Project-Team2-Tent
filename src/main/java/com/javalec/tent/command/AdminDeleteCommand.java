1ae4f729857455338cae121cbf2ad80ad8b84578
wookhyunKim <kwh1041@naver.com>
2023-06-01 18:08:29 +0900

    update/deleteAction add

--- src/main/java/com/javalec/tent/command/AdminDeleteCommand.java
+++ src/main/java/com/javalec/tent/command/AdminDeleteCommand.java
@@ -4,19 +4,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.javalec.tent.dao.AdminDao;
-import com.javalec.tent.dto.AdminDto;
 
-public class AdminContentCommand implements TentCommand {
+public class AdminDeleteCommand implements TentCommand {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
 		String pCode =request.getParameter("pCode");
+		String pColor =request.getParameter("pColor");
+		
+		
 		
 		AdminDao dao  = new AdminDao();
-		
-		AdminDto dto = dao.contentView(pCode);
-		request.setAttribute("content_view", dto);
+		dao.deleteAction(pCode, pColor);
 
 	}
 
