befa489af1f16863b452a28def1c553bd73258ae
seola <132555534+parkseola89@users.noreply.github.com>
2023-06-02 14:41:35 +0900

    dao, dto수정

--- src/main/java/com/javalec/tent/command/PurchaserInfoCommand.java
+++ src/main/java/com/javalec/tent/command/PurchaserInfoCommand.java
@@ -1,7 +1,13 @@
 package com.javalec.tent.command;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.javalec.tent.dao.PurchaseDao;
+import com.javalec.tent.dto.PurchaseDto;
 
 public class PurchaserInfoCommand implements TentCommand {
 
@@ -9,6 +15,17 @@ public class PurchaserInfoCommand implements TentCommand {
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
 
+			// TODO Auto-generated method stub
+			HttpSession session = request.getSession();
+			String uid = (String)session.getAttribute("SUID");
+			PurchaseDao dao = new PurchaseDao();
+		
+			ArrayList<PurchaseDto> dtos = dao.selectUser(uid);
+			request.setAttribute("selectUser", dtos);
+
+		}
+
 	}
 
-}
+		
+		
--- src/main/java/com/javalec/tent/dao/PurchaseDao.java
+++ src/main/java/com/javalec/tent/dao/PurchaseDao.java
@@ -1,6 +1,99 @@
 package com.javalec.tent.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import com.javalec.tent.dto.PurchaseDto;
 
 public class PurchaseDao {
 
+	//Field
+
+	
+	DataSource dataSource;
+	
+public PurchaseDao() {
+	// TODO Auto-generated constructor stub
+	try {
+		Context context = new InitialContext();
+		dataSource = (DataSource) context.lookup("java:comp/env/jdbc/tent");
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
 }
+
+
+
+     public ArrayList<PurchaseDto> selectUser(String uid) {
+ 		ArrayList<PurchaseDto> beanList = new ArrayList<PurchaseDto>();
+ 		Connection connection = null;
+ 		PreparedStatement preparedStatement = null;
+ 		ResultSet resultSet = null;
+
+ 		try {
+ 			connection = dataSource.getConnection();
+ 			String query = "select uName, uPhone, uEmail from user where uid = ?";
+ 			preparedStatement = connection.prepareStatement(query); 
+ 			preparedStatement.setString(1, uid);
+ 			resultSet = preparedStatement.executeQuery();
+
+ 			while (resultSet.next()) {
+ 				String uName = resultSet.getString(1);
+ 				String uPhone = resultSet.getString(2);
+ 				String uEmail = resultSet.getString(3);
+ 				PurchaseDto dto = new PurchaseDto(uid, uName, uPhone, uEmail);
+ 				beanList.add(dto);
+ 			}
+
+ 		} catch (Exception e) {
+ 			e.printStackTrace();
+ 		} finally {
+ 			try {
+ 				if (resultSet != null)
+ 					resultSet.close();
+ 				if (preparedStatement != null)
+ 					preparedStatement.close();
+ 				if (connection != null)
+ 					connection.close();
+ 			} catch (Exception e) {
+ 				e.printStackTrace();
+ 			}
+ 		}
+ 		return beanList;
+ 	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+}//END
+	
